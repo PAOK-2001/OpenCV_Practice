7c4d4f282084816291f17a719dfa28ab64f20fc5
Pablo <2001.paok@gmail.com>
2021-09-14 20:55:41 -0500

    Final version for Camera Feed script

--- CameraFeed/camera.cpp
+++ CameraFeed/camera.cpp
@@ -4,18 +4,17 @@
 #include <opencv2/core.hpp>
 #include <opencv2/videoio.hpp>
 #include <opencv2/highgui.hpp>
-
 #include <iostream>
 #include <stdio.h>
-
+#include <iostream>
+#include <stdio.h>
 using namespace cv;
 using namespace std;
-
 int main() {
     // Create OpenCV frame object to store frame information
     Mat frame;
-    // Create VideoCapture objet
-    VideoCapture camera(0);
+    // Create VideoCapture object, reading video device (USB camera)
+    VideoCapture camera(2);
     
     // Check if the camera is readable
     if(!camera.isOpened()){
@@ -25,13 +24,13 @@ int main() {
 
     for (;;){
         camera.read(frame);
-
+        // Check if selected device is sending information
         if(frame.empty()){
             cout<<"NULL frame ";
             break;
         }
         imshow("Camera Feed", frame);
-
+        // Read key board input, setting esc as break key
         if(waitKey(10)== 27){
             break;
         }
