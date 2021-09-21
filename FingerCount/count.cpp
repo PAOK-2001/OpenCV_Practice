cd52d3f6e3f177134a95ed2db918757a4e4cd3c6
Pablo <2001.paok@gmail.com>
2021-09-21 15:24:20 -0500

    Attepted to add background compensation. First attempt buggy

--- FingerCount/count.cpp
+++ FingerCount/count.cpp
@@ -16,17 +16,19 @@ using namespace cv;
 class Hand{
 private:
     Mat image;
+    Mat contrast;
     int fingers;
     bool isHand;
 public:
     Hand();
-    Hand(Mat frame);
+    Hand(Mat frame, Mat background);
     Mat returnImage();
 };
 
-Hand::Hand(Mat frame){
-    cvtColor(frame, frame, COLOR_BGR2GRAY );
-    threshold(frame, image,70,255,THRESH_BINARY);
+Hand::Hand(Mat frame, Mat background){
+    absdiff(frame,background, contrast);
+    cvtColor(contrast, contrast, COLOR_BGR2GRAY );
+    threshold(contrast, image,70,255,THRESH_OTSU);
 }
 
 
@@ -35,18 +37,19 @@ Mat Hand::returnImage(){
 }
 
 
-
 int main() {
+    bool hasBackground = false;
     // Create OpenCV frame object to store frame information
     Mat frame;
     Mat Region;
+    Mat background;
     // Scalar object represent red
     Scalar red = Scalar(0,0,255);
     // Rectangle that limits the Region of Interest (ROI)
     Rect ROI(410,45,225,225);
 
     // Create VideoCapture object, reading video device (USB camera)
-    VideoCapture camera(2);
+    VideoCapture camera(0);
     
     
     // Check if the camera is readable
@@ -56,23 +59,44 @@ int main() {
     }
 
     for (;;){
-        camera.read(frame);
-        // Check if selected device is sending information
-        if(frame.empty()){
-            cout<<"NULL frame ";
-            break;
+        if (hasBackground){
+            camera.read(frame);
+            // Check if selected device is sending information
+            if(frame.empty()){
+                cout<<"NULL frame ";
+                break;
+            }
+            rectangle(frame,ROI,red,5);
+            Hand h1(frame(ROI), background);
+            imshow("Camera Feed", frame);
+            imshow("Region of Interest", h1.returnImage());
+            // Read key board input, setting esc as break key
+            if(waitKey(10)== 27){
+                break;
+            }
         }
-        
-        rectangle(frame,ROI,red,5);
-        Hand h1(frame(ROI));
-        imshow("Camera Feed", frame);
-        imshow("Region of Interest", h1.returnImage());
-        // Read key board input, setting esc as break key
-        if(waitKey(10)== 27){
-            break;
+
+        if (!hasBackground){
+            camera.read(frame);
+            // Check if selected device is sending information
+            if(frame.empty()){
+                cout<<"NULL frame ";
+                break;
+            }
+            
+            rectangle(frame,ROI,red,5);
+            frame(ROI).copyTo(background);
+            Hand h1(frame(ROI),background);
+            imshow("Camera Feed", frame);
+            imshow("Region of Interest", h1.returnImage());
+            // Read key board input, setting esc as break key
+            if(waitKey(10)== 27){
+                break;
+            }
+            hasBackground = true;
         }
+
     }
-    
     return 0;
 }
 
